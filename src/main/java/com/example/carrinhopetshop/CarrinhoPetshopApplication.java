90a3ccef1bd4eb68871032ce03c64403ec5048eb
Gabriel Resende <gabriel.resende@develcode.com.br>
2023-11-07 09:20:15 -0300

    spring boot project created

---
+++ src/main/java/com/example/carrinhopetshop/CarrinhoPetshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrinhopetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoPetshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrinhoPetshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carrinhopetshop/CarrinhoPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrinhopetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoPetshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
