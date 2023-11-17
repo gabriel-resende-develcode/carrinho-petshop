de2530963fb9e6ae4ff0e63796bd23031e7d755c
Gabriel Resende <gabriel.resende@develcode.com.br>
2023-11-17 16:24:56 -0300

    test: CartItemServiceTest created

--- src/main/java/com/example/carrinhopetshop/model/CartItem.java
+++ src/main/java/com/example/carrinhopetshop/model/CartItem.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @Getter
+@Setter
 public class CartItem {
 
     @Id
--- src/test/java/com/example/carrinhopetshop/service/Implementation/CartServiceTest.java
+++ src/test/java/com/example/carrinhopetshop/service/Implementation/CartServiceTest.java
@@ -24,7 +24,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
